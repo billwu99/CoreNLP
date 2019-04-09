f5d1c87e9b89dbe260c69d9cb817bbb03940d7c2
billwu99 <billwu95@gmail.com>
2019-04-08 21:21:18 -0400

    Changed resources path

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -28,30 +28,35 @@ import edu.stanford.nlp.ling.CoreAnnotations.*;
 import edu.stanford.nlp.time.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException, ClassNotFoundException{
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
 
-        String[] text = {"", "h.to"};
+        String[] text = { "", "h.to" };
 
-        //trainAndWrite("model.txt", "test.properties", "testData.txt");
+        // trainAndWrite("model.txt", "test.properties", "testData.txt");
         CRFClassifier model = getModel("./model.txt");
         doTagging(model, text[1]);
 
         System.out.println();
         System.out.println("Training ColumnDataClassifier");
-        ColumnDataClassifier cdc = new ColumnDataClassifier("/home/bill/Desktop/testCoreNlp/classify.properties");
-        cdc.trainClassifier("/home/bill/Desktop/testCoreNlp/classify.txt");
-        Datum<String,String> d = cdc.makeDatumFromStrings(text);
+        ColumnDataClassifier cdc = new ColumnDataClassifier("./classify.properties");
+        cdc.trainClassifier("./classify.txt");
+        Datum<String, String> d = cdc.makeDatumFromStrings(text);
         System.out.printf("%s  ==>  %s (%.4f)%n", text[1], cdc.classOf(d), cdc.scoresOf(d).getCount(cdc.classOf(d)));
 
-        // for (String line : ObjectBank.getLineIterator("/home/bill/Desktop/testCoreNlp/classify.test", "utf-8")) {
-        //     Datum<String,String> d = cdc.makeDatumFromLine(line);
-        //     System.out.printf("%s  ==>  %s (%.4f)%n", line, cdc.classOf(d), cdc.scoresOf(d).getCount(cdc.classOf(d)));
+        // for (String line :
+        // ObjectBank.getLineIterator("/home/bill/Desktop/testCoreNlp/classify.test",
+        // "utf-8")) {
+        // Datum<String,String> d = cdc.makeDatumFromLine(line);
+        // System.out.printf("%s ==> %s (%.4f)%n", line, cdc.classOf(d),
+        // cdc.scoresOf(d).getCount(cdc.classOf(d)));
         // }
 
         // System.out.println();
         // System.out.println("Testing accuracy of ColumnDataClassifier");
-        // Pair<Double, Double> performance = cdc.testClassifier("/home/bill/Desktop/testCoreNlp/classify.test");
-        // System.out.printf("Accuracy: %.3f; macro-F1: %.3f%n", performance.first(), performance.second());
+        // Pair<Double, Double> performance =
+        // cdc.testClassifier("/home/bill/Desktop/testCoreNlp/classify.test");
+        // System.out.printf("Accuracy: %.3f; macro-F1: %.3f%n", performance.first(),
+        // performance.second());
         System.out.println();
 
         DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
@@ -65,16 +70,18 @@ public class Main {
         // props.setProperty("sutime.includeRange", "true");
         // StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
         // Annotation document = new Annotation(text[1]);
-        // document.set(CoreAnnotations.DocDateAnnotation.class, dateFormat.format(date));
+        // document.set(CoreAnnotations.DocDateAnnotation.class,
+        // dateFormat.format(date));
         // pipeline.annotate(document);
 
         // List<CoreMap> sentences = document.get(SentencesAnnotation.class);
 
         // for(CoreMap sentence: sentences){
-        //     for(CoreLabel token: sentence.get(TokensAnnotation.class)){
-        //         System.out.print(token.get(NamedEntityTagAnnotation.class) + "/" + token.get(NormalizedNamedEntityTagAnnotation.class) + " ");
-        //     }
-        //     System.out.println();
+        // for(CoreLabel token: sentence.get(TokensAnnotation.class)){
+        // System.out.print(token.get(NamedEntityTagAnnotation.class) + "/" +
+        // token.get(NormalizedNamedEntityTagAnnotation.class) + " ");
+        // }
+        // System.out.println();
         // }
         long time1 = System.currentTimeMillis();
 
@@ -89,7 +96,7 @@ public class Main {
         pipeline.addAnnotator(new WordsToSentencesAnnotator(false));
         pipeline.addAnnotator(new POSTaggerAnnotator(false));
         pipeline.addAnnotator(new TimeAnnotator("sutime", props));
-        
+
         Annotation annotation = new Annotation(text[1]);
         annotation.set(CoreAnnotations.DocDateAnnotation.class, dateFormat.format(date));
         pipeline.annotate(annotation);
@@ -100,14 +107,15 @@ public class Main {
             System.out.println(cm + " --> " + cm.get(TimeExpression.Annotation.class).getTemporal());
         }
         long time2 = System.currentTimeMillis();
-        System.out.println(time2-time1);
+        System.out.println(time2 - time1);
 
     }
-    public static void trainAndWrite(String modelOutPath, String prop, String trainingFilePath){
+
+    public static void trainAndWrite(String modelOutPath, String prop, String trainingFilePath) {
         Properties props = StringUtils.propFileToProperties(prop);
         props.setProperty("serializeTo", modelOutPath);
 
-        //if input use that, else use from properties file.
+        // if input use that, else use from properties file.
         if (trainingFilePath != null) {
             props.setProperty("trainFile", trainingFilePath);
         }
@@ -118,13 +126,14 @@ public class Main {
 
         crf.serializeClassifier(modelOutPath);
     }
+
     public static CRFClassifier getModel(String modelPath) {
         return CRFClassifier.getClassifierNoExceptions(modelPath);
     }
+
     public static void doTagging(CRFClassifier model, String input) {
         input = input.trim();
-        System.out.println(input + " => "  +  model.classifyToString(input, "inlineXML", true));
-    }            
-
+        System.out.println(input + " => " + model.classifyToString(input, "inlineXML", true));
+    }
 
 }
